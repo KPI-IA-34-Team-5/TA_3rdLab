17c62967180ca17e9243f6887fd8536a5dc7ad0c
GlebTiK <55922929+GlebTiK@users.noreply.github.com>
2024-05-09 20:24:47 +0300

    Update the algorithm + input/output

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from io import TextIOWrapper
 import statistics
 import sys
+import os
 from loguru import logger
 
 # https://github.com/Delgan/loguru/issues/138#issuecomment-1491571574
@@ -13,59 +14,44 @@ def min_level_filter(record):
 logger.remove()
 logger.add(sys.stderr, format="[{time:HH:mm:ss.SSS} | <level>{level: <8}</level>] {message}", filter=min_level_filter)
 
-def partition(array, low, high):
-    counter = 0
-    pivot = array[high]
+def partition(arr, low, high):
+    pivot = arr[high]
     i = low - 1
+    count = 0
     for j in range(low, high):
-        if array[j] <= pivot:
-            i = i + 1
-            (array[i], array[j]) = (array[j], array[i])
-        counter = counter + 1
-    (array[i + 1], array[high]) = (array[high], array[i + 1])
-    return i + 1, counter
- 
- 
-def quick_sort(array, low=0, high=None):
-    if high == None:
-        high = len(array) - 1
-    counter_1 = 0
-    counter_2 = 0
-    counter_3 = 0
+        count += 1
+        if arr[j] < pivot:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
+    arr[i + 1], arr[high] = arr[high], arr[i + 1]
+    return i + 1, count
+
+def quick_sort(arr, low, high):
     if low < high:
-        pi, counter_1 = partition(array, low, high)
-        counter_2 = quick_sort(array, low, pi - 1)
-        counter_3 = quick_sort(array, pi + 1, high)
-    return counter_1 + counter_2 + counter_3
+        pi, local_count = partition(arr, low, high)
+        left_count = quick_sort(arr, low, pi - 1)
+        right_count = quick_sort(arr, pi + 1, high)
+        return local_count + left_count + right_count
+    return 0
 
-
-def quick_sort_modified(array):
-    if len(array) <= 3:
-        return quick_sort(array)
-    a = array[0]
-    b = array[len(array)-1]
-    c = array[len(array) // 2]
-    
-    pivot = statistics.median([a,b,c])
-    
-    less = []
-    equal = []
-    greater = []
-    
-    counter = 0
-    
-    for x in array:
-        if x == pivot:
-            equal.append(x)
-            counter = counter + 1
-            continue
-        if x < pivot:
-            less.append(x)
-            counter = counter + 2
-            continue
-        greater.append(x)
-        counter = counter + 2    
-    return counter + quick_sort_modified(less) + quick_sort_modified(greater)
+def quick_sort_modified(arr):
+    count = 0
+    def partition_mod(arr, low, high):
+        mid = (low + high) // 2
+        pivot_candidates = [(arr[low], low), (arr[mid], mid), (arr[high], high)]
+        pivot_candidates.sort()
+        median_value, median_index = pivot_candidates[1]
+        arr[median_index], arr[high] = arr[high], arr[median_index]
+        return partition(arr, low, high)
+    def sort(arr, low, high):
+        nonlocal count
+        if low < high:
+            pi, local_count = partition_mod(arr, low, high)
+            count += local_count
+            sort(arr, low, pi - 1)
+            sort(arr, pi + 1, high)
+    sort(arr, 0, len(arr) - 1)
+    return count
 
 def file_reader(file: TextIOWrapper):
     lines = file.readlines()
@@ -81,14 +67,18 @@ def file_reader(file: TextIOWrapper):
     return arr
 
 if __name__ == "__main__":
-    logger.info("Starting to work..")
-    inp = file_reader(open("./input.txt"))
-    logger.debug("Working with quicksort..")
-    qs_out = quick_sort(list(inp))
-    logger.debug("Working with quicksort modified..")
-    qs_m_out = quick_sort_modified(list(inp))
-    logger.info("Finsihed working. Writing to file.")
-    out = str(qs_out) + " " + str(qs_m_out)
-    f = open("./output.txt", "w")
-    f.write(out)
-    f.close()
+    directory = os.getcwd() + "\\input"
+
+    for filename in os.listdir(directory):
+        if filename.startswith('input'):
+            with open(directory + "\\" + filename, 'r') as file:
+                logger.info(f"Starting to work with {filename}")
+                inp = file_reader(file)
+                logger.debug("Working with quicksort..")
+                qs_out = quick_sort(list(inp), 0, len(inp) - 1)
+                logger.debug("Working with quicksort modified..")
+                qs_m_out = quick_sort_modified(list(inp))
+                logger.info(f"Finsihed working. Writing to file {filename.replace('input', 'output')}.")
+                out = str(qs_out) + " " + str(qs_m_out)
+                with open(os.getcwd() + "\\output\\" + filename.replace('input', 'output'), "w") as f:
+                    f.write(out)
